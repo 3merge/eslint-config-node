457d13726f30c70461c13cc65d4049a8f9c8359c
Mike <mibberson@3merge.ca>
2019-09-16 08:57:11 -0400

    release: linter setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# eslint-config-node
-Importable ESLint configuration
+# ESlint Node
+
+3merge styleguide for Node projects. Refer to [Airbnb](https://github.com/airbnb/javascript) for specifics.
---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+    'jest/globals': true,
+    es6: true
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  plugins: [
+    'prettier',
+    'jest'
+  ],
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+    ecmaFeatures: {
+      esversion: 9,
+      destructuring: true,
+      generators: true,
+      superInFunctions: true,
+      experimentalObjectRestSpread: true,
+    }
+  },
+  rules: {
+    'prettier/prettier': 1,
+    'implicit-arrow-linebreak': 0,
+    'linebreak-style': 0,
+    'eol-last': 0,
+    'function-paren-newline': 0,
+    'no-underscore-dangle': 0,
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true
+      }
+    ],
+  }
+}
