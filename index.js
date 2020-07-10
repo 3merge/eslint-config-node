1a5eea27a5c87fc111a6cb5941086ffca2a2f364
Mike <mibberson@3merge.ca>
2020-07-09 21:27:19 -0400

    fix: jest/globals no longer recognized

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
 module.exports = {
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   env: {
     node: true,
     browser: true,
     jest: true,
-    'jest/globals': true,
-    es6: true
+    es6: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
-  plugins: [
-    'prettier',
-    'jest'
-  ],
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier", "jest"],
   parserOptions: {
     ecmaVersion: 9,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
       esversion: 9,
       destructuring: true,
       generators: true,
       superInFunctions: true,
       experimentalObjectRestSpread: true,
-    }
+    },
   },
   rules: {
-    'prettier/prettier': 1,
-    'implicit-arrow-linebreak': 0,
-    'linebreak-style': 0,
-    'eol-last': 0,
-    'function-paren-newline': 0,
-    'no-underscore-dangle': 0,
+    "prettier/prettier": 1,
+    "implicit-arrow-linebreak": 0,
+    "linebreak-style": 0,
+    "eol-last": 0,
+    "function-paren-newline": 0,
+    "no-underscore-dangle": 0,
     quotes: [
-      'error',
-      'single',
+      "error",
+      "single",
       {
-        avoidEscape: true
-      }
+        avoidEscape: true,
+      },
     ],
-  }
-}
+  },
+};
